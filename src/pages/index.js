e445bc2e7a4a140195476503e7a99a7563c8e052
Fatfullin <fatmax@iMac-Fatfullin.Dlink>
2018-09-23 23:47:59 +0300

    2 videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,15 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Kick off your project with this default boilerplate. This barebones starter ships with the main Gatsby     configuration files you might need.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+
   </Layout>
 )
 
