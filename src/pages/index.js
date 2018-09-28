9f75aee3791c7840946f84651545a6cb311d1407
Fatfullin <fatmax@iMac-Fatfullin.Dlink>
2018-09-28 03:00:10 +0300

    Step-5

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,26 @@ const IndexPage = () => (
         <p>Welcome to your new Gatsby site.</p>
         <p>Kick off your project with this default boilerplate. This barebones starter ships with the main Gatsby     configuration files you might need.</p>
         <Link to="/page-2/">Go to page 2</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')} width="50"/>
+          <img src={require('../images/logo-figma.png')} width="50"/>
+          <img src={require('../images/logo-studio.png')} width="50"/>
+          <img src={require('../images/logo-framer.png')} width="50"/>
+          <img src={require('../images/logo-react.png')} width="50"/>
+          <img src={require('../images/logo-swift.png')} width="50"/>
+        </div>
+        
+        <svg width="100%" height="172"  fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill="white">
+            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+            
+            M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
+
+            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
+            "
+            />
+          </path>        
+        </svg>
       </div>
     </div>
 
@@ -18,3 +38,4 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
